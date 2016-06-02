b0cbf0c888350727837bdcd0b3e7d7b1da4a20ea
fritz-c <fritz-c@users.noreply.github.com>
2016-06-02 14:28:55 +0900

    Change gulpfile indentation

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,37 @@ var gulpif = require('gulp-if');
 var connect = require('gulp-connect');
 
 function compile(dev) {
-  return gulp.src('./lib/*.coffee')
-    .once('data', function() {gutil.log('Compile: start');})
-    .once('end', function() {gutil.log('Compile: finish');})
-    .pipe(coffee({ bare: true }).on('error', gutil.log))
-    .pipe(gulp.dest('dist'))
-    .pipe(closureCompiler({
-      compilerPath: 'bower_components/closure-compiler/compiler.jar',
-      fileName: 'oms.min.js',
-      compilerFlags: {
-        compilation_level: 'ADVANCED_OPTIMIZATIONS',
-        externs: [
-          'bower_components/google-maps-externs/google_maps_api.js'
-        ],
-        output_wrapper: '(function(){%output%})();',
-        warning_level: 'QUIET'
-      }
-    }))
-    .pipe(gulp.dest('dist'))
-    .pipe(gulpif(dev, connect.reload()));
+    return gulp.src('./lib/*.coffee')
+        .once('data', function() {gutil.log('Compile: start');})
+        .once('end', function() {gutil.log('Compile: finish');})
+        .pipe(coffee({ bare: true }).on('error', gutil.log))
+        .pipe(gulp.dest('dist'))
+        .pipe(closureCompiler({
+            compilerPath: 'bower_components/closure-compiler/compiler.jar',
+            fileName: 'oms.min.js',
+            compilerFlags: {
+                compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                externs: [
+                    'bower_components/google-maps-externs/google_maps_api.js'
+                ],
+                output_wrapper: '(function(){%output%})();',
+                warning_level: 'QUIET'
+            }
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(gulpif(dev, connect.reload()));
 }
 
 gulp.task('default', ['connect'], function () {
-  gulp.watch('./examples/*.html', function() { gulp.src('./examples/*.html').pipe(connect.reload()); });
-  gulp.watch('./lib/*.coffee', function() { compile(true); });
-  compile(true);
+    gulp.watch('./examples/*.html', function() { gulp.src('./examples/*.html').pipe(connect.reload()); });
+    gulp.watch('./lib/*.coffee', function() { compile(true); });
+    compile(true);
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    livereload: true,
-  });
+    connect.server({
+        livereload: true,
+    });
 });
 
 gulp.task('build', compile);
