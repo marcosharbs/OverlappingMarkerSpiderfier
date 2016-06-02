66cf7658dc96620166fb3cb4c6a2c09a828374b4
fritz-c <fritz-c@users.noreply.github.com>
2016-06-02 17:53:44 +0900

    Use webpack instead of gulp

---
+++ server.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack');
+var webpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+var port = 3000;
+
+var compiler = webpack(config);
+var server = new webpackDevServer(compiler, {
+    hot: true,
+    noinfo: true,
+    publicPath: config.output.publicPath,
+});
+
+server.listen(port, function(error) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var env = process.env.NODE_ENV;
+
+var config = {
+    entry: [
+        './src/oms.coffee'
+    ],
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/static/',
+        filename: 'bundle.js',
+        library: 'OverlappingMarkerSpiderfier',
+        libraryTarget: 'umd',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.coffee$/,
+                loader: 'coffee',
+                exclude: /node_modules/,
+                include: __dirname,
+            }
+        ]
+    }
+};
+
+if (env === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                pure_getters: true,
+                screw_ie8: true,
+                warnings: false,
+            }
+        })
+    );
+} else {
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    );
+
+    config.entry.unshift(
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/dev-server'
+    );
+}
+
+module.exports = config;
