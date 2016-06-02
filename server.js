6a2f5474e8ef68e8e624855825880a7b53b823d6
fritz-c <fritz-c@users.noreply.github.com>
2016-06-02 18:23:16 +0900

    Fix dev server hint

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ server.listen(port, function(error) {
     if (error) {
         console.error(error);
     } else {
-        console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+        console.info('==> Listening on port %s. Open up http://localhost:%s/examples/ in your browser.', port, port);
     }
 });
